22926f79b992786378682c5e363385ee797b56e8
0r10n650 <orion4cooler@gmail.com>
2026-09-14 20:30:09 -0600

    Added Marble to Start Block

--- UnityProjects/Assets/Scripts/MarbleScripts/MarbleSystem.cs
+++ UnityProjects/Assets/Scripts/MarbleScripts/MarbleSystem.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
+using System;
 
 public class MarbleSystem : MonoBehaviour
 {
     [SerializeField] Rigidbody marble;
+    public static event Action OnSimulationStart;
+
+    public static void TriggerSimulation()
+    {
+        OnSimulationStart?.Invoke();
+    }
 
     public void Start()
     {
         marble.useGravity = false;
     }
+    private void OnEnable()
+    {
+        OnSimulationStart += BeginSimulation;
+    }
 
+    private void OnDisable()
+    {
+        OnSimulationStart -= BeginSimulation;
+    }
     public void BeginSimulation()
     {
         print("trying");
